bdecc7db7038726d1e1a9bb30901199bfd8499ae
Yehor Lvivski <lvivski@gmail.com>
2012-08-09 23:43:12 +0300

    initial commit

---
+++ Source/chm_lib.m
@@ -0,0 +1,13 @@
+//
+//  chm_lib.m
+//  chm_lib
+//
+//  Created by Yehor Lvivski on 4/7/12.
+//  Copyright (c) 2012 Arivo Solutions. All rights reserved.
+//
+
+#import "chm_lib.h"
+
+@implementation chm_lib
+
+@end
---
+++ build.sh
@@ -0,0 +1,3 @@
+cd Source
+xcodebuild build -configuration Release -target chm_lib SYMROOT=../build/ OBJROOT=../build
+
